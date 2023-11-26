b00728a5fe72aef9df12c95769eb79fff0db9822
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2023-11-25 22:03:59 -0300

    *

---
+++ KafkaAppEvent/src/main/java/com/example/KafkaAppEvent/KafkaAppEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAppEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAppEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAppEventApplication.class, args);
+	}
+
+}
---
+++ KafkaAppEvent/src/test/java/com/example/KafkaAppEvent/KafkaAppEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KafkaAppEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAppEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

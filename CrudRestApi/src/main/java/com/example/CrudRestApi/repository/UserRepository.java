022dbd20db8625cc1a30f04ae11186f2b5a06fc0
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2023-11-27 14:25:08 -0300

    Microservice

--- CrudRestApi/src/main/java/com/example/CrudRestApi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CrudRestApi.repository;
-
-import com.example.CrudRestApi.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- blog-rest-api/src/main/java/com/myblog/blogrestapi/payload/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.myblog.blogrestapi.payload;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String usernameOrEmail;
-    private String password;
-}

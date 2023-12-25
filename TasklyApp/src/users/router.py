f3aa429ad990f510deb49f2ce4350464f80250bd
Sukuna963 <86301498+Sukuna963@users.noreply.github.com>
2023-12-25 15:07:29 -0300

    Delete TasklyApp directory

--- TasklyApp/src/users/router.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .viewsets import UserViewSets
-
-app_name = "users"
-
-router = routers.DefaultRouter()
-router.register('users',UserViewSets)
--- TasklyApp/src/users/viewsets.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from .serializers import UserSerializer
-from .permissions import IsUserOwnerOrGetAndPostOnly
-
-class UserViewSets(viewsets.ModelViewSet):
-    permission_classes = [IsUserOwnerOrGetAndPostOnly,]
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
